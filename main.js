dc26b5d2808d2077f69e3b3172da1b2bc8c2d836
sidbird <123426974+sidbird@users.noreply.github.com>
2023-02-06 19:14:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,36 @@ function startClassification() {
 function modelReady(){
     classifier.classify(gotresults);
 }
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+var dog=0;
+var cat=0;
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - '+results[0].label;
+        document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")"; 
+document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")"; 
+
+img=document.getElementById('animal_image');
+    
+if(results[0].label=="Barking"){
+    img.src = 'dog.jpg';
+    dog = dog+1;
+}else if(results[0].label=="Meowing"){
+    img.src = 'cat.jpg';
+    cat = cat+1;
+}else{
+    img.src = 'ear image.png';
+}
+}
+}
