22648c9e6b8e1f2a8e0bb5757174333cc645ce4e
sidbird <123426974+sidbird@users.noreply.github.com>
2023-02-06 20:02:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ function startClassification() {
     navigator.mediaDevices.getUserMedia({
         audio: true
     });
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
 }
 
 function modelReady(){
-    classifier.classify(gotresults);
+    classifier.classify(gotResults);
 }
+
 
 function modelReady(){
     classifier.classify(gotResults);
